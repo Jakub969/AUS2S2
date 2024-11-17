3cf7104d1a3eaa212cba3cc1466d96a502530dc7
Jakub969 <jakub.hrubizna@gmail.com>
2024-11-17 21:45:46 +0100

    implementovanie bloku a haldoveho suboru

--- src/Blok.java
+++ src/Blok.java
@@ -1,27 +1,74 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Blok<T extends IZaznam<T>> implements IByteOperacie {
     private int pocetValidnychZaznamov;
-    private IZaznam<T>[] zaznamy;
+    private ArrayList<IZaznam<T>> zaznamy;
+    private int pocetZaznamov;
     private int dalsiVolnyIndex;
+    private int predchadzajuciVolnyIndex;
 
-    public Blok(int pocetValidnychZaznamov, IZaznam<T>[] zaznamy, int dalsiVolnyIndex) {
-        this.pocetValidnychZaznamov = pocetValidnychZaznamov;
-        this.zaznamy = zaznamy;
-        this.dalsiVolnyIndex = dalsiVolnyIndex;
+    public Blok(int blokovaciFaktor) {
+        this.zaznamy = new ArrayList<>(blokovaciFaktor);
+        this.pocetZaznamov = blokovaciFaktor;
     }
 
     @Override
     public void fromByteArray(byte[] poleBytov) {
+        ByteArrayInputStream hlpByteArrayInputStream = new ByteArrayInputStream(poleBytov);
+        DataInputStream hlpInStream = new DataInputStream(hlpByteArrayInputStream);
 
+        try {
+            this.pocetValidnychZaznamov = hlpInStream.readInt();
+            this.dalsiVolnyIndex = hlpInStream.readInt();
+            this.predchadzajuciVolnyIndex = hlpInStream.readInt();
+            int velkostZaznamu = zaznamy.getFirst().getSize();
+            int offset = Integer.BYTES + Integer.BYTES + Integer.BYTES;
+            for (int i = 0; i < pocetZaznamov; i++) {
+                byte[] zaznamBytes = new byte[velkostZaznamu];
+                hlpInStream.read(zaznamBytes, 0, velkostZaznamu);
+                IZaznam<T> zaznam = zaznamy.get(i);
+                zaznam.fromByteArray(zaznamBytes);
+                offset += velkostZaznamu;
+            }
+
+        } catch (IOException e) {
+            throw new IllegalStateException("Chyba pri konverzii z byte array.");
+        }
     }
 
     @Override
     public byte[] toByteArray() {
-        return new byte[0];
+        ByteArrayOutputStream hlpByteArrayOutputStream = new ByteArrayOutputStream();
+        DataOutputStream hlpOutStream = new DataOutputStream(hlpByteArrayOutputStream);
+
+        try {
+            hlpOutStream.writeInt(this.pocetValidnychZaznamov);
+            hlpOutStream.writeInt(this.dalsiVolnyIndex);
+            hlpOutStream.writeInt(this.predchadzajuciVolnyIndex);
+            int velkostZaznamu = zaznamy.getFirst().getSize();
+            int offset = Integer.BYTES + Integer.BYTES + Integer.BYTES;
+            for (IZaznam<T> z : zaznamy) {
+                byte[] zaznamBytes = z.toByteArray();
+                hlpOutStream.write(zaznamBytes, 0, zaznamBytes.length);
+                offset += velkostZaznamu;
+            }
+            return hlpByteArrayOutputStream.toByteArray();
+
+
+        } catch (IOException e) {
+            throw new IllegalStateException("Chyba pri konverzii do byte array.");
+        }
     }
 
     @Override
     public int getSize() {
-        return 0;
+        int velkostZaznamu = zaznamy.getFirst().getSize();
+        return Integer.BYTES + Integer.BYTES + Integer.BYTES + (velkostZaznamu * pocetZaznamov);
     }
 
     public T getZaznam(T zaznam) {
@@ -33,7 +80,46 @@ public class Blok<T extends IZaznam<T>> implements IByteOperacie {
         return null;
     }
 
-    public int predchadzajuciBlok() {
-        return 0;
+    public void vlozZaznam(IZaznam<T> zaznam) {
+    if (pocetValidnychZaznamov < pocetZaznamov) {
+        zaznamy.add(zaznam);
+        pocetValidnychZaznamov++;
+    } else {
+        throw new IllegalStateException("Blok je plný.");
+    }
+    }
+
+    public void zmazZaznam(IZaznam<T> zaznam) {
+        for (IZaznam<T> z : zaznamy) {
+            if (z.rovnaSa(zaznam)) {
+                zaznamy.remove(z);
+                pocetValidnychZaznamov--;
+                return;
+            }
+        }
+    }
+
+    public int getDalsiVolnyIndex() {
+        return dalsiVolnyIndex;
+    }
+
+    public void setDalsiVolnyIndex(int dalsiVolnyIndex) {
+        this.dalsiVolnyIndex = dalsiVolnyIndex;
+    }
+
+    public int getPredchadzajuciVolnyIndex() {
+        return predchadzajuciVolnyIndex;
+    }
+
+    public void setPredchadzajuciVolnyIndex(int predchadzajuciVolnyIndex) {
+        this.predchadzajuciVolnyIndex = predchadzajuciVolnyIndex;
+    }
+
+    public int getPocetValidnychZaznamov() {
+        return pocetValidnychZaznamov;
+    }
+
+    public void setPocetValidnychZaznamov(int pocetValidnychZaznamov) {
+        this.pocetValidnychZaznamov = pocetValidnychZaznamov;
     }
 }
--- src/HaldovySubor.java
+++ src/HaldovySubor.java
@@ -1,14 +1,46 @@
-public class HaldovySubor<T> {
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class HaldovySubor<T extends IZaznam<T>> {
     private int uplnePrazdnyBlok;
     private int ciastocnePrazdnyBlok;
+    private int pocetZaznamov;
+    private Class<T> typZaznamu;
+    private String nazovSuboru;
 
-    public HaldovySubor(int uplnePrazdnyBlok, int ciastocnePrazdnyBlok) {
-        this.uplnePrazdnyBlok = uplnePrazdnyBlok;
-        this.ciastocnePrazdnyBlok = ciastocnePrazdnyBlok;
+    public HaldovySubor(String nazovSuboru, Class<T> typZaznamu, int blokovaciFaktor) {
+        this.pocetZaznamov = blokovaciFaktor;
+        this.typZaznamu = typZaznamu;
+        this.nazovSuboru = nazovSuboru;
     }
     public void vlozZaznam(IZaznam<T> zaznam) {
+        Blok<T> blok = getBlok(ciastocnePrazdnyBlok);
 
+        if (blok.getPocetValidnychZaznamov() >= pocetZaznamov) {
+            ciastocnePrazdnyBlok = blok.getDalsiVolnyIndex();
+            blok = getBlok(ciastocnePrazdnyBlok);
+        }
+        blok.vlozZaznam(zaznam);
+
+        if (blok.getPocetValidnychZaznamov() == pocetZaznamov) {
+            uplnePrazdnyBlok = ciastocnePrazdnyBlok;
+            ciastocnePrazdnyBlok = blok.getDalsiVolnyIndex();
+        }
     }
+
+    private Blok<T> getBlok(int indexBloku) {
+    try (RandomAccessFile file = new RandomAccessFile(nazovSuboru, "r")) {
+        long offset = indexBloku * (long) new Blok<>(pocetZaznamov).getSize();
+        byte[] blokBytes = new byte[new Blok<>(pocetZaznamov).getSize()];
+        file.seek(offset);
+        file.readFully(blokBytes);
+        Blok<T> blok = new Blok<>(pocetZaznamov);
+        blok.fromByteArray(blokBytes);
+        return blok;
+    } catch (IOException e) {
+        throw new IllegalStateException("Chyba pri načítaní bloku.", e);
+    }
+}
 
     public T getZaznam(T zaznam, int blok) {
         return null;
